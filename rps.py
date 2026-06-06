7fe35bcc9d196a69b5de882493459f3b403b7bf1
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-06-06 12:03:44 +1000

    updated with game logic

--- rps.py
+++ rps.py
@@ -18,3 +18,14 @@ print("")
 print("You chose " + playerchoice + ".")
 print("Python chose " + computerchoice + ".")
 print("")
+
+if player == 1 and computer == 3:
+    print("🎉 You win!")
+elif player == 2 and computer == 1:
+    print("🎉 You win!")
+elif player == 3 and computer == 2:
+    print("🎉 You win!")
+elif player == computer:
+    print("😮 Tie game!")
+else:
+    print("🐍 Python wins!")
