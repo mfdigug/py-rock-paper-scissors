12f89742229f8e34894dfd61e7989f59b5bb230b
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-06-07 17:55:55 +1000

    updated rps with function

--- examples.py
+++ examples.py
@@ -1,5 +1,11 @@
-value = True
+value = "y"
+count = 0
 
-while value: # equivalent of while value exists or while value == True
-    print(value)
-    value = False
+while value: # equivalent of 'while value exists' or while value == True
+    count += 1
+    print(count)
+    if (count == 5): # this code won't be reached due to the else continue statement
+        break
+    else:
+        value = 0
+        continue
